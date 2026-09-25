d95aa23f43c9415fcf64dcc30fa4b651728f41cb
KrisztianKirschner <95145322+KrisztianKirschner@users.noreply.github.com>
2026-09-25 09:33:48 +0200

    feat: update Proxmox template with fixed cloud image version and adjust VM configuration

--- terraform/proxmox/template.tf
+++ terraform/proxmox/template.tf
@@ -2,12 +2,12 @@ resource "proxmox_download_file" "cloud_image" {
   content_type = "import"
   datastore_id = "local"
   node_name    = "pve"
-  url          = "https://cloud-images.ubuntu.com/resolute/current/resolute-server-cloudimg-amd64.img"
+  url          = "https://cloud-images.ubuntu.com/resolute/20260921/resolute-server-cloudimg-amd64.img"
   file_name    = "resolute-server-cloudimg-amd64.qcow2"
 }
 
 resource "proxmox_virtual_environment_vm" "template" {
-  name      = "Ubuntu-server-2604-template"
+  name      = "ubuntu-2604-template"
   vm_id     = 9999
   node_name = "pve"
   template  = true
@@ -15,8 +15,8 @@ resource "proxmox_virtual_environment_vm" "template" {
   machine   = "q35"
 
   cpu {
-    cores = 4
-    type  = "x86-64-v2-AES"
+    cores = 2
+    type  = "host"
   }
 
   memory { dedicated = 4096 }
@@ -26,15 +26,25 @@ resource "proxmox_virtual_environment_vm" "template" {
     import_from  = proxmox_download_file.cloud_image.id
     interface    = "virtio0"
     size         = 20
+    discard      = "on"
+    iothread     = true
   }
 
   network_device { bridge = "vmbr1" }
+
+  serial_device {}
+  vga { type = "serial0" }
 
   initialization {
     ip_config {
       ipv4 {
         address = "dhcp"
       }
+    }
+
+    user_account {
+      username = "proxima"
+      keys     = [var.ssh_public_key]
     }
   }
 }
