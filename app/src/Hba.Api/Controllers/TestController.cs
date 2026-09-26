fe6f69cccf509cb916fb4b0178a30fc7dc76bab0
KrisztianKirschner <95145322+KrisztianKirschner@users.noreply.github.com>
2026-09-26 14:35:43 +0200

    feat: add TestController with a simple GET endpoint and configure API services

---
+++ app/src/Hba.Api/Controllers/TestController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Hba.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        // GET: api/Test
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello from hba-api");
+        }
+    }
+}
--- app/src/Hba.Api/Program.cs
+++ app/src/Hba.Api/Program.cs
@@ -1,6 +1,8 @@
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();
+builder.Services.AddOpenApi();
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
@@ -8,5 +10,7 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();              // /openapi/v1.json
     app.MapScalarApiReference();   // /scalar
 }
+
+app.MapControllers();
 
 app.Run();
